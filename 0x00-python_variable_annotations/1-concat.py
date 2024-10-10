a412a9a38f6e2f62a9752090eccb1a8e0a60a62a
rallmac <tobijahekperikpe@gmail.com>
2024-10-10 17:03:50 +0100

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" This function concats strings taken as arguments
+    and returns the concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """After concatenating the string, the
+       concatenated streing is returned
+    """
+    return str1 + str2
