46d31fc01bb1cbb7ce5362ca18a214341c6eaf0c
rallmac <tobijahekperikpe@gmail.com>
2024-10-10 17:46:10 +0100

    Task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""This is a type annotated function to string
+   this function takes in a float n as an argument
+   and returns a string reptesenting the float
+"""
+
+
+def to_str(n: float) -> str:
+    """After taking the float, the function
+       returns a string
+    """
+    return str(n)
