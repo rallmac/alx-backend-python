384266817c8aea702ac6c2790eeabb34702b9cfe
rallmac <tobijahekperikpe@gmail.com>
2024-10-10 18:17:11 +0100

    task 4

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This function defines variables with specific
+   values
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
