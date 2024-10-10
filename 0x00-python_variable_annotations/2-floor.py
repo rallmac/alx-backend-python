dd0a3af19144004a14858a2caf39d85abcc39e35
rallmac <tobijahekperikpe@gmail.com>
2024-10-10 17:22:41 +0100

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""This function takes in floor annotation
+   and returns the floor argument as float
+"""
+
+import math
+
+def floor(n: float) -> float:
+    """The float would now be returned
+       after the annotation
+    """
+    return math.floor(n)
