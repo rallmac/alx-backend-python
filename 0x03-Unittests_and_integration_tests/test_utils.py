315c21275386a0e24edc6311b1c6aeb0ac18db22
rallmac <tobijahekperikpe@gmail.com>
2024-11-02 10:31:42 +0100

    task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,7 +14,17 @@ class TestAccessNestedMap(unittest.TestCase):
             ({"a": {"b": 2}}, ("a", "b"), 2),
         ])
     def test_access_nested_map(self, nested_map, path, expected):
+        """This method tests the parameterized expand function"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+        @parameterized.expand([
+            ({}, ("a",)),
+            ({"a": 1}, ("a", "b")),
+        ])
+        def test_access_nested_map_exception(self, nested_map, path):
+            with self.assertRaises(KeyError) as context:
+                access_nested_map(nested_map, path)
+            self.assertEqual(str(context.exception), f"'{path[-1]}'")
 
 
 if __name__ == '__main__':
